6c349b2eb849de7c5504c250a8165bd66586364f
Cedric Ziel <cedric@cedric-ziel.com>
2023-08-07 15:41:40 +0200

    Extend faro base settings

--- lib/src/flutter_faro_settings.dart
+++ lib/src/flutter_faro_settings.dart
@@ -1,7 +1,8 @@
+import 'package:faro_dart/faro_dart.dart';
 import 'package:flutter_faro/src/resource_detector.dart';
 
-class FlutterFaroSettings {
+class FlutterFaroSettings extends FaroSettings {
   ResourceDetector? resourceDetector;
 
-  Uri? collectorUrl;
+  FlutterFaroSettings({Uri? collectorUrl}): super(collectorUrl: collectorUrl);
 }
